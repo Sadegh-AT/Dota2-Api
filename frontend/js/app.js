65935754065999302cd9190b004189072fa642ca
SadeghAT <sadegh.artivox@gmail.com>
2023-01-24 22:53:51 +0330

    validate input when null

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -5,6 +5,7 @@ const typeImage = document.querySelector(".type-image");
 const image = document.querySelector(".image img");
 const healthCount = document.querySelector(".health-count");
 const manaCount = document.querySelector(".mana-count");
+const container = document.querySelector(".container");
 
 let myData;
 form.addEventListener("submit", (event) => {
@@ -12,11 +13,11 @@ form.addEventListener("submit", (event) => {
   fetch(`http://localhost:2000/api/dota2-hero?name=${input.value}`)
     .then((response) => response.json())
     .then((data) => {
-      heroName.textContent = data.name;
-      typeImage.src = getType(data.type);
-      image.src = data.img;
-      healthCount.textContent = data.health;
-      manaCount.textContent = data.mana;
+      if (data != "Null") {
+        SetTextAndImage(data);
+      } else {
+        errorStyle(input);
+      }
     });
 });
 
@@ -28,4 +29,38 @@ function getType(type) {
   } else if (type === "int") {
     return "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_intelligence.png";
   }
+}
+
+function SetTextAndImage(data) {
+  const card = document.querySelector(".hero-card");
+  if (card) {
+    card.remove();
+  }
+  container.insertAdjacentHTML(
+    "beforeend",
+    `<div class="hero-card">
+            <div class="name">${data.name}</div>
+            <div class="type">
+                <img class="type-image" src="${getType(data.type)}" alt="${
+      data.type
+    }">
+            </div>
+            <div class="image">
+                <img src="${data.img}" alt="${data.name}">
+            </div>
+            <div class="healthbar bar">
+                <span class="health-count">${data.health}</span>
+            </div>
+            <div class="manabar bar">
+                <span class="mana-count">${data.mana}</span>
+            </div>
+        </div>`
+  );
+}
+
+input.addEventListener("input", function () {
+  this.classList.remove("error");
+});
+function errorStyle(input) {
+  input.classList.add("error");
 }
