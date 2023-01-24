5bc26727eac5b8b7646fd4684feca2c1474ea9e8
SadeghAT <sadegh.artivox@gmail.com>
2023-01-24 21:57:45 +0330

    make api

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,33 @@
 const http = require("http");
 const url = require("url");
-
+const heroes = require("./heroes.json");
 const server = http.createServer((req, res) => {
-  if (req.url == "/api/dota2-hero") {
-    res.end("ok");
+  const pathUrl = url.parse(req.url, true);
+  console.log();
+
+  if (req.method == "GET" && pathUrl.pathname === "/api/dota2-hero") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.end(JSON.stringify(getHero(pathUrl.query.name)));
+  } else {
+    res.end("404");
   }
 });
 
 server.listen(2000);
+
+function getHero(name) {
+  const hero = heroes.filter((hero) => hero.localized_name == name);
+  if (hero[0]) {
+    let myHero = {
+      id: hero[0].id,
+      name: hero[0].localized_name,
+      type: hero[0].primary_attr,
+      img: hero[0].img,
+      health: hero[0].base_health,
+      mana: hero[0].base_mana,
+    };
+    return myHero;
+  } else {
+    return "Null";
+  }
+}
--- frontend/js/app.js
+++ frontend/js/app.js
@@ -3,4 +3,7 @@ const input = document.querySelector("input");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  fetch(`http://localhost:2000/api/dota2-hero?name=${input.value}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 });
